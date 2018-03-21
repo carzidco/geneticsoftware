800dac02e0c882bb8b04e822a7bd5f721269200d
charlie <carzidco@hotmail.com>
2018-03-20 22:15:12 -0600

    add new chat

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1468,7 +1468,7 @@
     (function(){
     var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
     s1.async=true;
-    s1.src='https://embed.tawk.to/5a948d134b401e45400d3904/default';
+    s1.src='https://embed.tawk.to/5ab1db29d7591465c708c12c/default';
     s1.charset='UTF-8';
     s1.setAttribute('crossorigin','*');
     s0.parentNode.insertBefore(s1,s0);
